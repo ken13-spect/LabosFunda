e0a644db1d641a252f333bb1f50b5d1386350110
ken13-spect <62628516+ken13-spect@users.noreply.github.com>
2020-05-10 22:21:30 -0600

    Add files via upload

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int  main() {
+  float a, b, c, x;
+   cout<<"Ingrese un valor para a: " ;
+   cin>>a;
+   cout<<"ingrese un valor para b: " ;
+   cin>>b;
+   cout<<"Ingrese un valor para c: " ;
+   cin>>c;
+
+   x  = ( a + b + c)/3;
+
+   cout<<"El promedio es: "<<x;
+     
+     return 0;
+}
